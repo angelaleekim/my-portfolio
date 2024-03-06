8b76e929da754b3880894ae2b2da43567ced3a3f
angelakim <akim173@calpoly.edu>
2024-03-05 17:43:08 -0800

    Updated project page content

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,10 @@
 // Project Objects //
+import cp from "../assets/images/cp.jpg";
+import slobg from "../assets/images/slo-bg.jpg";
+import unc from "../assets/images/uncluttered-logo-text-blue.jpg";
+import cpdemo from "../assets/videos/cp-demo.mp4";
+import slobgdemo from "../assets/videos/slobg-demo.mp4";
+import uncdemo from "../assets/videos/unc-demo.mov";
 
 const projects = [
   {
@@ -10,13 +16,9 @@ const projects = [
 
 As a frontend developer, some of my tasks include wireframing our project in Figma, creating frontend React components such as the welcome and login pages, and styling our entire application with Tailwind. `,
     technologies: ["React", "TailwindCSS", "Figma", "Node.js"],
-    demo: "https://private-user-images.githubusercontent.com/62783989/310289743-4d530ba7-339d-4615-a969-025f06a48b4a.mov?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDk2NzgwMzQsIm5iZiI6MTcwOTY3NzczNCwicGF0aCI6Ii82Mjc4Mzk4OS8zMTAyODk3NDMtNGQ1MzBiYTctMzM5ZC00NjE1LWE5NjktMDI1ZjA2YTQ4YjRhLm1vdj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzA1VDIyMjg1NFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTIzMGU4N2IwNmQxNzFjOTg1MDNiZmI0N2I1NmM2YzVmMWFiODBiZjkxYTZlZmNlYzQxMGFkYzkxZDc0NjJlYjYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0._giWGOaExP3530TzkUEqR_JmCcSAYTNiGtl450HaLPA",
+    demo: uncdemo,
     code: "https://github.com/angelaleekim/mlflow-demo",
-    images: [
-      "https://private-user-images.githubusercontent.com/62783989/310288753-1cc3dc43-10e0-465a-a232-69c0ef2ef792.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDk2NzU0NTUsIm5iZiI6MTcwOTY3NTE1NSwicGF0aCI6Ii82Mjc4Mzk4OS8zMTAyODg3NTMtMWNjM2RjNDMtMTBlMC00NjVhLWEyMzItNjljMGVmMmVmNzkyLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzA1VDIxNDU1NVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWNlN2Y4OWNlM2I4MTQyZTM1ODM4MGQ4NzgxMmY5NTQyZjk5YTI1ODA2ZWQyZDIyMmYwZDMyMDVhMzM5ZGIzMGQmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.FoOpRZXj95nnhtuKNqCMTRhDRkj30bD0iYO57Bdzb90",
-      "https://user-images.githubusercontent.com/62783989/246952635-af1ad0b6-4ff5-4228-be02-9afcf9bf12c9.png",
-      "https://user-images.githubusercontent.com/62783989/246952715-b50d93ec-36fe-4e16-8db5-f87bd04544ec.png",
-    ],
+    images: [unc],
   },
   {
     title: "Mindbody + Classpass",
@@ -27,13 +29,9 @@ As a frontend developer, some of my tasks include wireframing our project in Fig
 
 Additionally, I led an impactful A/B testing experiment focused on optimizing the send time for the ClassPass campaign. Through rigorous experimentation and analysis of user engagement data, I successfully determined the optimal time to send campaigns to individual users, leading to improved campaign effectiveness and higher user conversions. This experience not only honed my data analysis skills but also provided valuable insights into user behavior and campaign optimization strategies.`,
     technologies: ["Python", "SQL", "Amazon S3", "Docker"],
-    demo: "https://user-images.githubusercontent.com/62783989/250471902-825f21b9-7957-427c-a1e5-b80db8a78c85.mp4",
+    demo: cpdemo,
     code: "https://github.com/angelaleekim/mlflow-demo",
-    images: [
-      "https://private-user-images.githubusercontent.com/62783989/310302182-6c2cf53b-e792-4b2d-971d-852edbdb3cfc.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDk2NzkwODQsIm5iZiI6MTcwOTY3ODc4NCwicGF0aCI6Ii82Mjc4Mzk4OS8zMTAzMDIxODItNmMyY2Y1M2ItZTc5Mi00YjJkLTk3MWQtODUyZWRiZGIzY2ZjLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzA1VDIyNDYyNFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJhYTY5MmRlN2ZiYmI2MmI0NGZjMjcwOTVlYzE3OWJkNTBlMjE0N2YyMzAyNDNkYmQxOGJlMjJlNDI5N2VjMjgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.ocH_Gp4w4f9rclMgkYte2oX4CYzMVxXYn1XBAuzPypQ",
-      "https://user-images.githubusercontent.com/62783989/246952635-af1ad0b6-4ff5-4228-be02-9afcf9bf12c9.png",
-      "https://user-images.githubusercontent.com/62783989/246952715-b50d93ec-36fe-4e16-8db5-f87bd04544ec.png",
-    ],
+    images: [cp],
   },
   {
     title: "SLO Botanical Garden",
@@ -44,13 +42,9 @@ Additionally, I led an impactful A/B testing experiment focused on optimizing th
       
 This experience not only sharpened my technical skills but also allowed me to cultivate strong communication and collaboration abilities.By working together harmoniously, we created a platform that has greatly improved the efficiency and impact of volunteer management for the SLO Botanical Garden, positively impacting the community we serve.`,
     technologies: ["React", "Node.js", "MongoDB"],
-    demo: "https://user-images.githubusercontent.com/62783989/248520364-d007bb96-cb7a-425a-96cf-f2d9fa8c3bad.mp4",
+    demo: slobgdemo,
     code: "https://github.com/hack4impact-calpoly/sloBotanicalGarden22",
-    images: [
-      "https://private-user-images.githubusercontent.com/62783989/310301566-b5754c04-5bd9-434f-aea1-88d4ba762662.jpg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDk2Nzg4NzgsIm5iZiI6MTcwOTY3ODU3OCwicGF0aCI6Ii82Mjc4Mzk4OS8zMTAzMDE1NjYtYjU3NTRjMDQtNWJkOS00MzRmLWFlYTEtODhkNGJhNzYyNjYyLmpwZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAzMDUlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMzA1VDIyNDI1OFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTIzNDY2ZGZmM2E0ZDUwNTgyZjM4YmI1NTM2YjIzZTYyZGNiZDE3Y2JjZmI4NjJjMzhjMjdlOTcwNzhhNjUzNTImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.OsgO69QuXs9SIiEKgzpyiZRIPxCKU8r8kjjh53e2foU",
-      "https://user-images.githubusercontent.com/62783989/246949270-639c07c6-e986-4c06-a03f-6b1f1014cd1f.png",
-      "https://user-images.githubusercontent.com/62783989/246949340-da954349-f063-43a4-b928-5ca47311b560.png",
-    ],
+    images: [slobg],
   },
 ];
 
